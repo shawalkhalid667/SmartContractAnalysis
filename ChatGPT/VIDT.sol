f57a4b1721a5552983a9c04f44fb2ae4b010475c
Shawal Khalid <98194914+shawalkhalid667@users.noreply.github.com>
2024-04-23 17:21:33 -0400

    Create VIDT.sol

---
+++ ChatGPT/VIDT.sol
@@ -0,0 +1,267 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.8;
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+        return c;
+    }
+
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+/**
+ * @title Context
+ * @dev Provides information about the message sender and transaction initiator.
+ */
+contract Context {
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+}
+
+/**
+ * @title Ownable
+ * @dev Provides ownership control functionalities.
+ */
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() public {
+        _owner = _msgSender();
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(_msgSender() == _owner, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+/**
+ * @title Pausable
+ * @dev Allows for emergency stopping of token transfers.
+ */
+contract Pausable is Ownable {
+    bool private _paused;
+
+    event Paused(address account);
+    event Unpaused(address account);
+
+    constructor() public {
+        _paused = false;
+    }
+
+    function paused() public view returns (bool) {
+        return _paused;
+    }
+
+    modifier whenNotPaused() {
+        require(!_paused, "Pausable: paused");
+        _;
+    }
+
+    modifier whenPaused() {
+        require(_paused, "Pausable: not paused");
+        _;
+    }
+
+    function pause() public onlyOwner whenNotPaused {
+        _paused = true;
+        emit Paused(_msgSender());
+    }
+
+    function unpause() public onlyOwner whenPaused {
+        _paused = false;
+        emit Unpaused(_msgSender());
+    }
+}
+
+/**
+ * @title ERC20
+ * @dev Simplified ERC20 interface
+ */
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function decimals() external view returns (uint8);
+    function symbol() external view returns (string memory);
+    function name() external view returns (string memory);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @title VIDT
+ * @dev VIDT Datalink token contract with extended functionalities
+ */
+contract VIDT is Context, Ownable, Pausable, IERC20 {
+    using SafeMath for uint256;
+
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
+    uint256 private _totalSupply;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => bool) private _frozenAccounts;
+
+    // Custom events
+    event AccountFrozen(address indexed account);
+    event AccountUnfrozen(address indexed account);
+    event Validation(address indexed validator, string indexed documentHash, bool isValid);
+    event BatchTransfer(address indexed sender, address indexed recipient, uint256 amount);
+
+    constructor(string memory name, string memory symbol, uint8 decimals, uint256 initialSupply) public {
+        _name = name;
+        _symbol = symbol;
+        _decimals = decimals;
+        _totalSupply = initialSupply * 10 ** uint256(decimals);
+        _balances[_msgSender()] = _totalSupply;
+        emit Transfer(address(0), _msgSender(), _totalSupply);
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override whenNotPaused returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override whenNotPaused returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+        return true;
+    }
+
+    function freezeAccount(address account) public onlyOwner {
+        _frozenAccounts[account] = true;
+        emit AccountFrozen(account);
+    }
+
+    function unfreezeAccount(address account) public onlyOwner {
+        _frozenAccounts[account] = false;
+        emit AccountUnfrozen(account);
+    }
+
+    function isAccountFrozen(address account) public view returns (bool) {
+        return _frozenAccounts[account];
+    }
+
+    function batchTransfer(address[] memory recipients, uint256[] memory amounts) public whenNotPaused returns (bool) {
+        require(recipients.length == amounts.length, "Recipient count does not match amount count");
+        uint256 senderBalance = _balances[_msgSender()];
+        for (uint256 i = 0; i < recipients.length; i++) {
+            address recipient = recipients[i];
+            uint256 amount = amounts[i];
+            require(recipient != address(0), "ERC20: transfer to the zero address");
+            require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+            _balances[_msgSender()] = senderBalance.sub(amount);
+            _balances[recipient] = _balances[recipient].add(amount);
+            emit Transfer(_msgSender(), recipient, amount);
+            emit BatchTransfer(_msgSender(), recipient, amount);
+        }
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(!_frozenAccounts[sender], "ERC20: sender account is frozen");
+        require(!_frozenAccounts[recipient], "ERC20: recipient account is frozen");
+        require(amount > 0, "Transfer amount must be greater than zero");
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        _balances[sender] = senderBalance.sub(amount);
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+}
