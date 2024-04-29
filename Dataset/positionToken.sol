76c7cb2431c5014c8124962ea39f43cb212db796
Shawal Khalid <98194914+shawalkhalid667@users.noreply.github.com>
2024-04-29 02:46:49 -0400

    Create positionToken.sol

---
+++ Dataset/positionToken.sol
@@ -0,0 +1,45 @@
+pragma solidity 0.8.25;
+
+// Fulcrum position token
+interface PositionToken {
+  function mintWithToken(
+    address receiver,
+    address depositTokenAddress,
+    uint256 depositAmount,
+    uint256 maxPriceAllowed)
+    external
+    returns (uint256);
+
+  function burnToToken(
+    address receiver,
+    address burnTokenAddress,
+    uint256 burnAmount,
+    uint256 minPriceAllowed)
+    external
+    returns (uint256);
+
+  function tokenPrice()
+   external
+   view
+   returns (uint256 price);
+
+  function liquidationPrice()
+   external
+   view
+   returns (uint256 price);
+
+  function currentLeverage()
+    external
+    view
+    returns (uint256 leverage);
+
+  function decimals()
+    external
+    view
+    returns (uint8);
+
+  function balanceOf(address account)
+    external
+    view
+    returns (uint256);
+}
